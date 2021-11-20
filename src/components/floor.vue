ea062391d2f2b17a4e181c71a13cebfb88a3abf6
derJunker <mail@jonas-heitz.eu>
2021-11-20 21:47:41 +0100

    helft mir ich gebe auf

--- src/components/floor.vue
+++ src/components/floor.vue
@@ -1,21 +1,29 @@
 <template>
-  <v-carousel :show-arrows="false" hide-delimiters vertical v-model="model">
-    <v-carousel-item v-for="(color, i) in colors" :key="color">
+  <v-carousel :show-arrows="false" hide-delimiters vertical v-model="model" >
+    <v-carousel-item v-for="(color,i) in colors" :key="color">
       <v-sheet :color="color" height="100%" tile>
-        <v-row class="fill-height" align="center" justify="center">
-          <div class="text-h2">Slide {{ max - i }}</div>
-          <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+        <v-row class="fill-height" align="center" justify="center" >
+          <div class="test">
+          <svg viewBox="0 0 500 250" xmlns="http://www.w3.org/2000/svg">
   <rect x="89.106" y="78.005" width="364.713" height="169.331" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
   <rect x="121.078" y="109.568" width="55.655" height="43.221" style="fill: rgb(198, 12, 12);"/>
   <circle style="fill: none;" cx="135.287" cy="123.002" r="19.331"/>
-  <image x="137.063" y="121.226" v-if="avatarId!==0" href="../assets/default_avatar.png" @click="changeDisplay(0)" width="26.051" height="23.091"/>
-  <image x="137.063" y="121.226" v-if="avatarId===0" href="../assets/user_avatar.png" @click="changeDisplay(0)" width="26.051" height="23.091"/>
+  <image x="137.063" y="121.226" v-if="etage !== i || avatarId!==0" href="../assets/default_avatar.png" @click="changeDisplay(0, i)" width="26.051" height="23.091"/>
+  <image x="137.063" y="121.226" v-if="avatarId===0 && etage === i" href="../assets/user_avatar.png" @click="changeDisplay(0, i)" width="26.051" height="23.091"/>
 </svg>
+          </div>
         </v-row>
       </v-sheet>
     </v-carousel-item>
   </v-carousel>
 </template>
+<style >
+.test{
+
+display: flex;
+}
+
+</style>
 
 <script>
 export default {
@@ -24,24 +32,32 @@ export default {
     this.$root.$on("doSlide", (slideNumber) => {
       this.model = slideNumber;
     });
+
+    this.$root.$on("daySelected", (date) => {
+      this.wochentag = date
+    })
   },
   data: () => ({
     max: 3,
     model: 3,
     colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
-    avatarId:-1
+    avatarId:-1,
+    etage:-1,
+    wochentag: new Date()
+    
   }),
 
    methods: {
-    changeDisplay(index) {
-      if(this.avatarId === index)
-        this.avatarId = -1;
-      else
-      this.avatarId = index;
+    changeDisplay(index, tempEtage) {
+      if(this.avatarId === index && this.etage === tempEtage){
+        this.avatarId = -1
+        this.etage = -1
+      } else {
+        this.avatarId = index
+        this.etage = tempEtage
+      }
+      
     },
   },
 };
 </script>
-
-<style>
-</style>
