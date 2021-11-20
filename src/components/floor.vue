4d28c50c02630a405e45b558858f40e511050876
moritjo <johannes.moritz@msg.group>
2021-11-20 22:41:17 +0100

    Revert "huhu"
    This reverts commit 9d6f68344a9b2f3e35d8b48d7d73f58da65ad57b, reversing
    changes made to 496c522642563f5e7baf44718a18ed56779bf415.

--- src/components/floor.vue
+++ src/components/floor.vue
@@ -4,7 +4,6 @@
       <v-sheet :color="color" height="100%" tile>
         <v-row class="fill-height" align="center" justify="center">
           <div class="test">
-<<<<<<< HEAD
             <svg viewBox="0 0 500 250" xmlns="http://www.w3.org/2000/svg">
               <rect
                 x="89.106"
@@ -40,15 +39,6 @@
                 height="23.091"
               />
             </svg>
-=======
-          <svg viewBox="0 0 500 250" xmlns="http://www.w3.org/2000/svg">
-  <rect x="89.106" y="78.005" width="364.713" height="169.331" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"/>
-  <rect x="121.078" y="109.568" width="55.655" height="43.221" style="fill: rgb(198, 12, 12);"/>
-  <circle style="fill: none;" cx="135.287" cy="123.002" r="19.331"/>
-  <image x="137.063" y="121.226" v-if="!isUserSelected(0, i)" href="../assets/default_avatar.png" @click="changeDisplay(0, i)" width="26.051" height="23.091"/>
-  <image x="137.063" y="121.226" v-if="isUserSelected(0, i)" href="../assets/user_avatar.png" @click="changeDisplay(0, i)" width="26.051" height="23.091"/>
-</svg>
->>>>>>> 7dbbbc0fa4a7ed3b1114a14e79483206520bd94c
           </div>
         </v-row>
       </v-sheet>
@@ -79,36 +69,20 @@ export default {
     max: 3,
     model: 3,
     colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
-    wochentag: new Date(),
-    wochenBuchMap: {
-      1: [-1, -1],
-      2: [-1, -1],
-      3: [-1, -1],
-      4: [-1, -1],
-      5: [-1, -1],
-      6: [-1, -1],
-      0: [-1, -1],
-    }
+    avatarId:-1,
+    etage:-1,
+    wochentag: new Date()
+    
   }),
 
    methods: {
-
-    isUserSelected(id, tempEtage) {
-      const tuple = this.wochenBuchMap[this.wochentag.getDay()]
-      const avatarId = tuple[0]
-      const etage = tuple[1]
-      return avatarId == id && etage == tempEtage
-    },
-
     changeDisplay(index, tempEtage) {
-      const tuple = this.wochenBuchMap[this.wochentag.getDay()]
-      const avatarId = tuple[0]
-      const etage = tuple[1]
-      // console.log(" etage: " + etage + " avatarId: " + avatarId + " wochentag: " + JSON.stringify(this.wochenBuchMap))
-      if(avatarId === index && etage === tempEtage){
-        this.wochenBuchMap[this.wochentag.getDay()] = [-1, -1]
+      if(this.avatarId === index && this.etage === tempEtage){
+        this.avatarId = -1
+        this.etage = -1
       } else {
-        this.wochenBuchMap[this.wochentag.getDay()] = [index, tempEtage]
+        this.avatarId = index
+        this.etage = tempEtage
       }
       
     },
