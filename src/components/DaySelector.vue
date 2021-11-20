9cf75a55b2d004986125f2c6c0437b8c4f1a4944
lfiebig <fiebiglennart@googlemail.com>
2021-11-20 19:02:36 +0100

    daySelector erstellt

---
+++ src/components/DaySelector.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="days">
+  <v-app id="inspire">
+    <v-card
+      flat
+      class="py-12"
+    >
+      <v-card-text>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-btn-toggle
+            v-model="selectedDay"
+            mandatory>
+            
+            <v-btn v-for="day in days" 
+                  :key="day" 
+                  id="btn1"
+                  @click="onDaySelected"
+                  class = "zoom"
+                  >
+                <v-text>{{day}}</v-text>
+            </v-btn>
+          </v-btn-toggle>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</div>
+
+</template>
+
+<script>
+  export default {
+    name: 'DaySelector',
+    methods: {
+      onDaySelected() {
+        var selectedDate = new Date()
+        this.$nextTick(()=> {
+          selectedDate.setDate(selectedDate.getDate() + this.selectedDay);
+          this.$root.$emit("daySelected", selectedDate)
+        })       
+      }
+    },
+    /*mounted() {
+      this.$root.$on("daySelected", (selectedDate) => {
+        console.log(selectedDate);
+      })
+    },*/
+    data: function() {
+
+      return {
+        days: ["SO","MO","DI","MI","DO","FR","SA"],
+        selectedDay: null,        
+      }
+    },
+    created() {
+      var currentDay = (new Date()).getDay()
+      var days1 = this.days.splice(currentDay)
+      var days2 = this.days.splice(0, currentDay)
+      this.days = days1.concat(days2)
+
+      //this.$root.$emit("daySelected", new Date())
+    }
+  }
+
+</script>
+
+<style scoped>
+  #btn1{
+    width: 100px;  
+    transition: transform .2s;
+
+}
+  #btn1:hover {
+  transform: scale(1.2); 
+  opacity: 100;
+  z-index: 99;
+}
+</style>
